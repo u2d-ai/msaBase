55df44e3de80e9e2e97c10f63d148e7956c804d7
SekachVitaliy <vitaly.sekach@gmail.com>
2023-11-13 14:19:27 +0200

    SAM-535: fix process hangs after sending a message

--- msaBase/configurate.py
+++ msaBase/configurate.py
@@ -323,9 +323,9 @@ class MSAApp(FastAPI):
             topic_name: name of Kafka topic to which this message should be sent.
             service_name: the name of the service from which the call was made
         """
-        if topic_name or ENABLE_MESSAGE_QUEUE:
+        if topic_name and ENABLE_MESSAGE_QUEUE:
             try:
-                producer = self.producer if SAVE_ALL_MESSAGES_IN_QUEUE else KafkaUtils.get_producer()
+                producer = self.producer or KafkaUtils.get_producer()
                 data = f"[{service_name}]: " + message if service_name else message
                 serialized_value = KafkaUtils.serialize_value(data)
                 producer.produce(topic_name, serialized_value)
