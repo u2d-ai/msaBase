6a454c8459424e0dedc981b8ac7f4f9b295cb380
SekachVitaliy <50178320+SekachVitaliy@users.noreply.github.com>
2023-11-15 11:50:27 +0200

    SAM-540: update log message

--- msaBase/configurate.py
+++ msaBase/configurate.py
@@ -215,7 +215,7 @@ class MSAApp(FastAPI):
         Subscribes service to Kafka topic through which new configs will be received.
         """
         if not ENABLE_MESSAGE_QUEUE:
-            self.logger_info("Kafka is not enabled. Skipping Kafka consumer creation.")
+            self.logger_info(f"Kafka is not enabled. Skipping Kafka consumer creation for topic {SERVICE_TOPIC}.")
         else:
             threading.Thread(target=self._consume_kafka_messages, daemon=True).start()
 
