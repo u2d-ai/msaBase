7259cf8af67f724bfcb8756c392dd0e9bf792df8
SekachVitaliy <50178320+SekachVitaliy@users.noreply.github.com>
2023-11-15 11:21:46 +0200

    SAM-540: use linters

--- msaBase/configurate.py
+++ msaBase/configurate.py
@@ -15,7 +15,7 @@ import aiohttp
 import sentry_sdk
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from apscheduler.schedulers.background import BackgroundScheduler
-from confluent_kafka import KafkaException, Producer
+from confluent_kafka import Producer
 from fastapi import FastAPI, HTTPException
 from fastapi.encoders import jsonable_encoder
 from fastapi.exception_handlers import http_exception_handler
